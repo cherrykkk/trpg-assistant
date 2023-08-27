693628b15c0ae5fb6cbe96595fe458bc0b12d45d
cherrykkk <klranone@163.com>
2023-08-27 19:26:15 +0800

    fix: 物品系统交互优化

--- packages/client/src/views/components/EntityStorage.vue
+++ packages/client/src/views/components/EntityStorage.vue
@@ -1,66 +1,45 @@
 <template>
-  <div class="entity-storage">
-    <div v-for="(stack, i) in manager.storage.value" :key="i" class="cell-container">
-      <button
-        class="clickable-cell"
-        :class="{ 'drag-chosen': dragChosen.includes(i) }"
-        @click.right.prevent="handleRightClick(i)"
-        @click="handleClick(stack)"
-      >
-        {{ stack.name }}{{ (stack.num ?? 1) === 1 ? "" : `*${stack.num}` }}
-      </button>
+  <div class="entity-storage" ref="entityStorageRef">
+    <div class="entity-menu">
+      <div v-for="(stack, i) in manager.storage.value" :key="i" class="cell-container">
+        <button
+          class="clickable-cell"
+          :class="{ 'drag-chosen': dragChosen.includes(i), 'edit-chosen': stackInDialog === stack }"
+          @click.right.prevent="handleRightClick(i)"
+          @click="handleClick(stack)"
+        >
+          {{ stack.name }}{{ (stack.num ?? 1) === 1 ? "" : `*${stack.num}` }}
+        </button>
+      </div>
+      <div class="add-item-button">
+        <ElSelect placeholder="添加" @change="(info: ItemInfo)=>{manager.add(info.id);}" filterable>
+          <ElOption v-for="item in ENTITY_DATABASE" :key="item.id" :label="item.name" :value="item"
+        /></ElSelect>
+      </div>
     </div>
-    <div class="add-item-button">
-      <ElSelect
-        placeholder="添加"
-        @change="(info: ItemInfo)=>{manager.add(info.id); $emit('change',manager.toData())}"
-        filterable
-      >
-        <ElOption v-for="item in ENTITY_DATABASE" :key="item.id" :label="item.name" :value="item"
-      /></ElSelect>
-    </div>
-  </div>
-  <ElDialog class="external-panel" v-model="showDialog">
-    <template v-if="stackInDialog">
+    <div v-if="stackInDialog" class="entity-editor">
+      <h2>
+        {{ stackInDialog.name }}{{ (stackInDialog.num ?? 1) === 1 ? "" : `*${stackInDialog.num}` }}
+        <span style="font-weight: 500; font-size: 14px; margin: 40px">{{
+          manager.calculateWeight(stackInDialog)
+        }}</span>
+        <button @click="remove" style="float: right">删除</button>
+      </h2>
+      <p>{{ stackInDialog.description }}</p>
+      <EditCellVue title="备注" v-model="stackInDialog.note" />
       <div>
-        <el-descriptions>
-          <template #title>
-            {{ stackInDialog.name
-            }}{{ (stackInDialog.num ?? 1) === 1 ? "" : `*${stackInDialog.num}` }}
-            <span style="font-weight: 500; font-size: 14px; margin: 40px">{{
-              manager.calculateWeight(stackInDialog)
-            }}</span>
-          </template>
-          <el-descriptions-item>{{ stackInDialog.description }}</el-descriptions-item>
-        </el-descriptions>
+        <EditCellVue title="数量" v-model="stackInDialog.num" />
       </div>
-      <div class="item-object-editor">
-        <el-form>
-          <el-form-item label="额外描述：">
-            <el-input type="textarea" v-model="stackInDialog.note" />
-          </el-form-item>
-          <el-form-item label="数量">
-            <el-input-number label="数量" v-model="stackInDialog.num" />
-          </el-form-item>
-        </el-form>
-      </div>
-      <button
-        @click="
-          manager.remove(stackInDialog);
-          showDialog = false;
-        "
-      >
-        删除
-      </button>
-    </template>
-  </ElDialog>
-  <button v-if="dragChosen.length > 0">转移</button>
+    </div>
+    <button v-if="dragChosen.length > 0">转移</button>
+  </div>
 </template>
 <script lang="ts" setup>
 import { ENTITY_DATABASE, type ItemInfo, type StoredStackData } from "@trpg/shared";
 import { type PropType, ref, watch } from "vue";
 import { useEntityManager } from "@/utils/EntityManager";
-import { ElDialog, ElOption, ElSelect } from "element-plus";
+import { ElOption, ElSelect } from "element-plus";
+import EditCellVue from "./EditCell.vue";
 
 const props = defineProps({
   initStoredStackData: {
@@ -83,6 +62,16 @@ watch(
   { immediate: true }
 );
 
+watch(
+  () => manager.storage.value,
+  () => {
+    emits("change", manager.toData());
+  },
+  {
+    deep: true,
+  }
+);
+
 const dragChosen = ref<number[]>([]);
 function handleRightClick(key: number) {
   if (dragChosen.value.includes(key)) {
@@ -92,21 +81,49 @@ function handleRightClick(key: number) {
   }
 }
 
-const showDialog = ref(false);
 const stackInDialog = ref<null | (typeof manager.storage.value)[number]>(null);
 function handleClick(stack: (typeof manager.storage.value)[number]) {
   stackInDialog.value = stack;
-  showDialog.value = true;
+}
+
+const entityStorageRef = ref<HTMLDivElement>();
+addEventListener("click", (e) => {
+  if (entityStorageRef.value && !e.composedPath().includes(entityStorageRef.value)) {
+    stackInDialog.value = null;
+  }
+});
+
+function remove() {
+  if (!stackInDialog.value) return;
+
+  const index = manager.storage.value.findIndex((e) => e === stackInDialog.value);
+  manager.remove(stackInDialog.value);
+
+  let newInEditor = manager.storage.value[index - 1];
+  if (!newInEditor && manager.storage.value.length > 0) {
+    newInEditor = manager.storage.value[0];
+  }
+  stackInDialog.value = newInEditor;
 }
 </script>
 
 <style lang="less" scoped>
 .entity-storage {
   font-size: 12px;
-  display: flex;
-  flex-wrap: wrap;
-  min-height: 60px;
-  padding: 5px 10px;
+  .entity-menu {
+    display: flex;
+    flex-wrap: wrap;
+    min-height: 60px;
+    padding: 5px 10px;
+  }
+  .entity-editor {
+    padding: 2px 10px;
+    height: 200px;
+    overflow: auto;
+  }
+  .edit-chosen {
+    border: 3px solid black;
+  }
   .drag-chosen {
     border: 1px solid red;
   }
